543f047107d465942871e33366a22d52dd54c7f0
Kyle Knoepfel <knoepfel@fnal.gov>
2025-02-04 15:34:01 -0600

    Provide more helpful printout during concretization.

--- mpd/concretize.py
+++ mpd/concretize.py
@@ -424,8 +424,13 @@ def concretize_project(project_config, yes_to_all):
                 continue
             first_order_deps.add(dep.name)
 
-    tty.msg(gray("Adjusting specifications for package development"))
-    subprocess.run(["spack", "-e", local_env_dir, "add"] + list(first_order_deps))
+    new_roots = "Adding the following packages as top-level dependencies:"
+    for dep in sorted(first_order_deps):
+        new_roots += f"\n    - {dep}"
+    tty.msg(gray(new_roots))
+    subprocess.run(["spack", "-e", local_env_dir, "add"] + list(first_order_deps),
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
 
     tty.info(gray("Finalizing concretization"))
     remove_view(local_env_dir)
@@ -433,7 +438,10 @@ def concretize_project(project_config, yes_to_all):
         env.concretize()
         env.write()
 
-    subprocess.run(["spack", "-e", local_env_dir, "rm"] + list(packages.keys()))
+    # Lastly, remove the developed packages from the environment
+    subprocess.run(["spack", "-e", local_env_dir, "rm"] + list(packages.keys()),
+                   stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
     with env, env.write_transaction():
         env.concretize()
         env.write()
