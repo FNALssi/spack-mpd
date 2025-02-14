ece8e9c1920b507c7afeb8cf7b22813961257710
Kyle Knoepfel <knoepfel@fnal.gov>
2025-02-14 16:13:23 -0600

    Fix unit tests based on updated printout.

--- tests/test_mpd_new_project.py
+++ tests/test_mpd_new_project.py
@@ -46,9 +46,10 @@ def test_new_project_all_default_paths(with_mpd_init, tmp_path):
     mpd("ls")
     with new_project(name="a", cwd=cwd_a) as out:
         print(out)
-        assert f"build area: {cwd_a}/build" in out
-        assert f"local area: {cwd_a}/local" in out
-        assert f"sources area: {cwd_a}/srcs" in out
+        assert f"top     {cwd_a}" in out
+        assert f"build   {cwd_a}/build" in out
+        assert f"local   {cwd_a}/local" in out
+        assert f"sources {cwd_a}/srcs" in out
         assert "a" == config.selected_project()
 
         out = mpd("status")
@@ -62,9 +63,10 @@ def test_new_project_only_top_path(with_mpd_init, tmp_path):
     top_level_b = tmp_path / "b"
     with new_project(name="b", top=top_level_b) as out:
         mpd("ls")
-        assert f"build area: {top_level_b}/build" in out
-        assert f"local area: {top_level_b}/local" in out
-        assert f"sources area: {top_level_b}/srcs" in out
+        assert f"top     {top_level_b}" in out
+        assert f"build   {top_level_b}/build" in out
+        assert f"local   {top_level_b}/local" in out
+        assert f"sources {top_level_b}/srcs" in out
 
 
 def test_new_project_only_srcs_path(with_mpd_init, tmp_path):
@@ -72,9 +74,10 @@ def test_new_project_only_srcs_path(with_mpd_init, tmp_path):
     cwd_c = tmp_path / "c"
     srcs_c = tmp_path / "c_srcs"
     with new_project(name="c", srcs=srcs_c, cwd=cwd_c) as out:
-        assert f"build area: {cwd_c}/build" in out
-        assert f"local area: {cwd_c}/local" in out
-        assert f"sources area: {srcs_c}" in out
+        assert f"top     {cwd_c}" in out
+        assert f"build   {cwd_c}/build" in out
+        assert f"local   {cwd_c}/local" in out
+        assert f"sources {srcs_c}" in out
 
 
 def test_new_project_no_default_paths(with_mpd_init, tmp_path):
@@ -82,9 +85,10 @@ def test_new_project_no_default_paths(with_mpd_init, tmp_path):
     top_level_d = tmp_path / "d"
     srcs_d = tmp_path / "d_srcs"
     with new_project(name="d", top=top_level_d, srcs=srcs_d) as out:
-        assert f"build area: {top_level_d}/build" in out
-        assert f"local area: {top_level_d}/local" in out
-        assert f"sources area: {srcs_d}" in out
+        assert f"top     {top_level_d}" in out
+        assert f"build   {top_level_d}/build" in out
+        assert f"local   {top_level_d}/local" in out
+        assert f"sources {srcs_d}" in out
 
 
 def test_mpd_refresh(with_mpd_init, tmp_path):
